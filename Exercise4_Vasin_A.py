87445fea06ec90989ebb55f5e3d2767da6d5b873
vsvs18 <159153402+vsvs18@users.noreply.github.com>
2024-02-06 22:06:45 +0700

    Add files via upload

---
+++ Exercise4_Vasin_A.py
@@ -0,0 +1,9 @@
+FE = 76.00
+GB = 80.00
+IC = 85.00
+CP = 86.00
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", IC)
+print("Computer Programming :", CP)
